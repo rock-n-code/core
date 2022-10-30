d2abad77d5e92e842662e095cb1930a75ebd62d0
Javier Cicchelli <javier@rock-n-code.com>
2022-10-30 20:02:59 +0100

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "Core",
+    products: [
+        .library(
+            name: "Core",
+            targets: [
+                "CoreSwift",
+                "CoreFoundation"
+            ]
+        ),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "CoreSwift",
+            path: "Sources/Swift"
+        ),
+        .target(
+            name: "CoreFoundation",
+            path: "Sources/Foundation"
+        ),
+        .testTarget(
+            name: "CoreTests",
+            dependencies: ["CoreSwift"],
+            path: "Tests/Swift"
+        ),
+        .testTarget(
+            name: "CoreFoundationTests",
+            dependencies: ["CoreFoundation"],
+            path: "Tests/Foundation"
+        )
+    ]
+)
