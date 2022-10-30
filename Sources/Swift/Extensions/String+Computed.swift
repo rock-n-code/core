3c33aaafee2d9a2328aab26d32872f4499a29073
Javier Cicchelli <javier@rock-n-code.com>
2022-10-30 20:25:07 +0100

    Implemented the "isNotEmpty" computed property for the String primitive type.

---
+++ Sources/Swift/Extensions/String+Computed.swift
@@ -0,0 +1,15 @@
+//
+//  String+Computed.swift
+//  CoreSwift
+//
+//  Created by Javier Cicchelli on 30/10/2022.
+//  Copyright © 2022 Röck+Cöde. All rights reserved.
+//
+
+public extension String {
+    
+    // MARK: Computed properties
+    
+    var isNotEmpty: Bool { !isEmpty }
+    
+}
---
+++ Tests/Swift/Extensions/String+IsNotEmptyTests.swift
@@ -0,0 +1,43 @@
+//
+//  String+IsNotEmptyTests.swift
+//  CoreSwiftTests
+//
+//  Created by Javier Cicchelli on 30/10/2022.
+//  Copyright © 2022 Röck+Cöde. All rights reserved.
+//
+
+import XCTest
+import CoreSwift
+
+final class StringIsNotEmptyTests: XCTestCase {
+
+    // MARK: Properties
+    
+    private var string: String!
+    private var result: Bool!
+    
+    // MARK: Tests
+    
+    func test_withEmptyString() throws {
+        // GIVEN
+        string = .empty
+        
+        // WHEN
+        result = string.isNotEmpty
+        
+        // THEN
+        XCTAssertFalse(result)
+    }
+    
+    func test_withNotEmptyString() throws {
+        // GIVEN
+        string = "Some string goes in here..."
+        
+        // WHEN
+        result = string.isNotEmpty
+        
+        // THEN
+        XCTAssertTrue(result)
+    }
+
+}
